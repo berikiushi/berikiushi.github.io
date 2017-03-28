e11528055477b59a3761dcc4b6221f1164c956f4
berikiushi <berikiushi@gmail.com>
2017-03-28 04:36:23 +0200

    add uggi to portfolio

---
+++ portfolio/uggi-official.ru/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+
+  // Open mobile menu
+  let mobileMenuOpenButton = document.getElementsByClassName('js-open-mobile-menu')[0];
+  let mobileMenu = document.getElementsByClassName('js-mobile-menu')[0];
+  let overlay = document.getElementsByClassName('js-overlay')[0];
+
+  mobileMenuOpenButton.addEventListener('click', openMobileMenu);
+  overlay.addEventListener('click', closeMobileMenu);
+
+  function openMobileMenu () {
+    mobileMenu.classList.add('_open');
+    overlay.classList.add('_show');
+  }
+
+  function closeMobileMenu () {
+    mobileMenu.classList.remove('_open');
+    overlay.classList.remove('_show');
+  }
+
+
+  // Slider
+  const mySiema = new Siema();
+
+}());
